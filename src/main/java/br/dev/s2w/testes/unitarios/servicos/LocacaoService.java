6ae66395fa4c0389f1faa7b4504c15c24f33e438
WybsonSantana <wybson@hotmail.com>
2023-01-21 13:46:33 -0300

    Adiciona o projeto inicial

---
+++ src/main/java/br/dev/s2w/testes/unitarios/servicos/LocacaoService.java
@@ -0,0 +1,34 @@
+package br.dev.s2w.testes.unitarios.servicos;
+
+import br.dev.s2w.testes.unitarios.entidades.Filme;
+import br.dev.s2w.testes.unitarios.entidades.Locacao;
+import br.dev.s2w.testes.unitarios.entidades.Usuario;
+
+import java.util.Date;
+
+import static br.dev.s2w.testes.unitarios.utils.DataUtils.adicionarDias;
+
+public class LocacaoService {
+
+    public Locacao alugarFilme(Usuario usuario, Filme filme) {
+        Locacao locacao = new Locacao();
+        locacao.setFilme(filme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+        locacao.setValor(filme.getPrecoLocacao());
+
+        //Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
+        //Salvando a locacao...
+        //TODO adicionar método para salvar
+
+        return locacao;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
