590f70cb9417f60274d1fee0c6177cbab68732b0
WybsonSantana <wybson@hotmail.com>
2023-01-25 15:13:19 -0300

    M2A08 - Formas de dividir um teste

--- src/test/java/br/dev/s2w/testes/unitarios/servicos/LocacaoServiceTest.java
+++ src/test/java/br/dev/s2w/testes/unitarios/servicos/LocacaoServiceTest.java
@@ -3,32 +3,36 @@ package br.dev.s2w.testes.unitarios.servicos;
 import br.dev.s2w.testes.unitarios.entidades.Filme;
 import br.dev.s2w.testes.unitarios.entidades.Locacao;
 import br.dev.s2w.testes.unitarios.entidades.Usuario;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
 import static br.dev.s2w.testes.unitarios.utils.DataUtils.isMesmaData;
 import static br.dev.s2w.testes.unitarios.utils.DataUtils.obterDataComDiferencaDias;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
 public class LocacaoServiceTest {
 
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+
     @Test
-    public void teste() {
+    public void testeLocacao() {
         // Cenário
         LocacaoService locacaoService = new LocacaoService();
         Usuario usuario = new Usuario("Usuário 1");
         Filme filme = new Filme("Filme 1", 2, 5.0);
 
-// Ação
+        // Ação
         Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
-// Verificação
-        assertThat(locacao.getValor(), is(equalTo(5.0)));
-        assertThat(locacao.getValor(), is(not(6.0)));
-        assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-        assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+        // Verificação
+        error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
     }
 
 
